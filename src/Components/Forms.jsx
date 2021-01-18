08c2ea7baefdf8f09f510b028734133fb7915983
Aleksandra Khabarova <sasha.khbrv@gmail.com>
2021-01-18 13:25:21 +0300

    Changed the name TextInputs -> TextInput

--- src/Components/Forms.jsx
+++ src/Components/Forms.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import TextInputs from './TextInputs/TextInputs';
+import TextInput from './TextInput/TextInput';
 
 function Forms() {
   return (
     <div className='row'>
       <div className="cell">
         <p className='cell-title'>&lt;Input /&gt;</p>
-        <TextInputs />
+        <TextInput />
       </div>
     </div>
   );
