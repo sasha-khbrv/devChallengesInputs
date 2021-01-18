25de04e151d1c3029d5306ba4704f3a2bf34f035
Aleksandra Khabarova <sasha.khbrv@gmail.com>
2021-01-18 19:23:26 +0300

    Added signature

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ function App() {
     <div className='container'>
       <h1>Inputs</h1>
       <Forms />
+      <div>
+        <p className="cell-title">
+          Made for <a href="https://devchallenges.io/" target="_blank" rel="noreferrer">DevChallenges</a> by <a href="https://github.com/sasha-khbrv?tab=repositories" target="_blank" rel="noreferrer">Sasha Khbrv</a>
+        </p>
+      </div>
     </div>
   );
 }
